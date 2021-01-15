6f48748c190c4a2ad008366519bbb19dc6ee9a87
Artur <pasartur108@gmail.com>
2021-01-15 10:00:36 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta http-equiv="Cache-control" content="public">
-    <meta name="theme-color" content="##2c2c2c">
+    <meta name="theme-color" content="#2c2c2c">
 
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css">
